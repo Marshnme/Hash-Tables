de9c77f9f1ba1adf5ca0f2e2cf3d8ef5ea79343e
Joshua Holtsclaw <jildsmiles@yahoo.com>
2020-03-09 19:09:41 -0400

    workworkworkworkworkwork

---
+++ src/hashes.py
@@ -0,0 +1,18 @@
+import hashlib
+
+n=1
+
+key=b"string"
+
+key2="string1".encode()
+
+
+index=hash(key) % 8
+# for i in range(n):
+#     print(hash(key))
+
+# for i in range(n):
+#     print(hash(key))
+
+# for i in range(n):
+#     print(hash(key2))
